738a436edfd2a252c2c18c7a4d0905e15e90bc5d
Ilan <ilan1il1000@gmail.com>
2022-03-14 17:48:11 +0200

    Done

--- mat.cpp
+++ mat.cpp
@@ -3,8 +3,13 @@
 #include <iostream>
 #include <vector>
 
+//algorithm from https://codegolf.stackexchange.com/questions/241219/mat-printing-matrix
+//answer by Jonah
+
 using namespace std;
 string ariel::mat(int col, int row, char c1, char c2){
+
+    //checking invalid input
     if (row % 2 == 0 || col % 2 == 0){
         throw invalid_argument("Row or column must be an odd number");
     }
@@ -16,16 +21,22 @@ string ariel::mat(int col, int row, char c1, char c2){
     if (c1 < minChar || c2 < minChar || c1 > maxChar || c2 > maxChar){
         throw invalid_argument("Invalid symbol");
     }
+
+    //creating 2 vector matrix
     vector<vector<int>> mat1(row, vector<int>(col));
     vector<vector<int>> mat2(row, vector<int>(col));
     string ans;
+
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < col; j++)
         {
-            mat1[i][j] = min(i, j);
-            mat2[i][j] = min(col - 1 - j, row - 1 - i);
-            mat1[i][j] = min(mat1[i][j], mat2[i][j]);
+            //applying values to the matrices
+            mat1.at(i).at(j) = min(i, j); 
+            mat2.at(i).at(j) = min(col - 1 - j, row - 1 - i);
+            //choosing the minmum value between the two matrices applying it to the 1st matrix to save storage
+            //also doing everything in the same loop to lower runtime complexity
+            mat1.at(i).at(j) = min(mat1.at(i).at(j), mat2.at(i).at(j)); 
         }
     }
 
@@ -33,7 +44,8 @@ string ariel::mat(int col, int row, char c1, char c2){
     {
         for (int j = 0; j < col; j++)
         {
-            if (mat1[i][j] % 2 == 0){
+            //applying the symbols to the string by modulu 2 of the value in the matrix
+            if (mat1.at(i).at(j) % 2 == 0){
                 ans += c1;
                 ans += " ";
             }
